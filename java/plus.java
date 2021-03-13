8332badded06839b1348342cce3e50e8f86ff9ab
Chaneun Yeo <55678293+LuneFleurs@users.noreply.github.com>
2021-03-13 16:55:45 +0900

    Create plus.java

---
+++ java/plus.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        while (true) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if (a == 0 && b == 0) {
+                break;
+            }
+            result.add(a + b);
+        }
+
+        result.forEach(elem -> System.out.println(elem));
+    }
+}
