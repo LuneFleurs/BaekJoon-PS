f5533106f0de41d7ae9c6831e4a4f402dfe60afc
Chaneun Yeo <55678293+LuneFleurs@users.noreply.github.com>
2021-09-01 12:25:14 +0900

    Create 10872.cpp

---
+++ cpp/10872.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n);
+
+int main(void) {
+	int n;
+	std::cin >> n;
+
+	std::cout << factorial(n);
+}
+
+int factorial(int n) {
+	if (n <= 1)
+		return 1;
+
+	return n * factorial(n - 1);
+}
