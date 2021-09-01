548452cbc628847583b61a18fd66706e9242fa01
Chaneun Yeo <55678293+LuneFleurs@users.noreply.github.com>
2021-09-01 12:17:39 +0900

    Create 4150.java

---
+++ java/4150.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int idx = sc.nextInt();
+
+        //System.out.println(Fibonacci(idx));
+        System.out.println(Fibonacci2(idx));
+    }
+
+    public static int Fibonacci(int idx) {
+        if(idx <= 1) {
+            return idx;
+        } else {
+            return Fibonacci(idx - 1) + Fibonacci(idx - 2);
+        }
+    }
+
+    public static BigInteger Fibonacci2(int idx) {
+        BigInteger[] fibonacciArr = new BigInteger[idx + 1];
+        fibonacciArr[0] = BigInteger.valueOf(0);
+
+        if (idx >= 1) {
+            fibonacciArr[1] = BigInteger.valueOf(1);
+            for (int i = 2; i <= idx; i++) {
+                fibonacciArr[i] = fibonacciArr[i-1].add(fibonacciArr[i-2]);
+            }
+        }
+        return fibonacciArr[idx];
+    }
+}
