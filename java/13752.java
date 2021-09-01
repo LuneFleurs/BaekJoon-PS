7cb089ea8d56c9f911b11823c42208ff9b540bdc
Chaneun Yeo <55678293+LuneFleurs@users.noreply.github.com>
2021-09-01 12:27:02 +0900

    Create 13752.java

---
+++ java/13752.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int cnt = sc.nextInt();
+        int [] arr = new int [cnt];
+        for (int i = 0; i < cnt; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+
+        for (int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr[i]; j++) {
+                System.out.print("=");
+            }
+            System.out.println("");
+        }
+    }
+}
