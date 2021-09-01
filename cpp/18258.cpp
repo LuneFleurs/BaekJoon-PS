6861a8b9451c96521e64fa855daeefc434666d1c
Chaneun Yeo <55678293+LuneFleurs@users.noreply.github.com>
2021-09-01 12:27:30 +0900

    Create 18258.cpp

---
+++ cpp/18258.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <queue>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    cin.tie(NULL);
+    cin.sync_with_stdio(NULL);
+    
+	int num, data;
+	queue<int> q; 
+    string str;
+	cin >> num;
+
+	for (int i = 0; i < num; i++) {
+		cin >> str;
+
+		if (str == "push") {
+			cin >> data;
+			q.push(data);
+		}
+		else if (str == "pop") {
+			if (q.size() == 0) std::cout << "-1\n";
+			else {
+				data = q.front();
+				q.pop();
+				cout << data << "\n";
+			}
+		}
+		else if (str == "size") {
+			cout << q.size() << "\n";
+		}
+		else if (str == "empty") {
+			cout << q.empty() << "\n";
+		}
+		else if (str == "front") {
+			if (q.size() == 0) std::cout << "-1\n";
+			else cout << q.front() << "\n";
+		}
+		else if (str == "back") {
+			if (q.size() == 0) std::cout << "-1\n";
+			else cout << q.back() << "\n";
+		}
+	}
+	return 0;
+}
